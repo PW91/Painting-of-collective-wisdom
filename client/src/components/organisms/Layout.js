650bc6ec937f323a394ace8a360e3528d8a43ca5
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2018-12-18 21:21:16 +0100

    up

--- client/src/components/organisms/Layout.js
+++ client/src/components/organisms/Layout.js
@@ -7,14 +7,9 @@ export default class Layout extends Component {
   render() {
     return (
       <div className="app">
-        <div className="desktop__app">
-          <FontsSampler />
-          <div className="wrap">{this.props.children}</div>
-          <MainFooter />
-        </div>
-        <div className="mobile__app">
-          <h1>Visit on desktop!</h1>
-        </div>
+        <FontsSampler />
+        <div className="wrap">{this.props.children}</div>
+        <MainFooter />
       </div>
     );
   }
